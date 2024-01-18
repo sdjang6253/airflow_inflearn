0d29d64d731b8735fa26e8bf1135d85d79902d88
sdjang6253 <qkswl625341@gmail.com>
2024-01-18 22:55:08 +0900

    op_args 추가

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,18 @@
+import datetime
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * 1",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    py_t1 = PythonOperator(
+        task_id='py_t1',
+        python_callable=regist,
+        op_args=['sdjang' , 'man' , 'kr' , 'seoul' ]
+    )
+    py_t1
