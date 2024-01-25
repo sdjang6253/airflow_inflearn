07dab4360ba30fb64e5e90284803fb38f026c325
sdjang6253 <qkswl625341@gmail.com>
2024-01-25 21:33:58 +0900

    테스트 코드 추가

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,20 @@
+import datetime
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.decorators import task
+
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="0 10 * * *",
+    start_date=pendulum.datetime(2024, 1, 11, tz="Asia/Seoul"),
+    catchup=True,
+) as dag:
+    
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_templates()
---
+++ dags/dags_python_template.py
@@ -0,0 +1,38 @@
+import pendulum
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+from airflow.decorators import dag, task
+
+with DAG(
+    dag_id="dags_python_template",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+   
+    def python_function1(start_date , end_date , **kwargs):
+        print(start_date)
+        print(end_date)
+    
+    python_t1 = PythonOperator(
+        task_id = 'python_t1',
+        python_callable=python_function1,
+        op_kwargs={'start_date':'{{date_interval_start | ds}}' , 'end_date' : '{{data_interval_end | ds}}'}
+    )
+
+    python_t1_2 = PythonOperator(
+        task_id = 'python_t1_2',
+        python_callable=python_function1,
+        op_args=['{{date_interval_start | ds}}' , '{{data_interval_end | ds}}']
+    )
+
+    @task(task_id='python_t2')
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds:' + kwargs['ds'])
+        print('ts:' + kwargs.get('ts')) #kwargs['ds'] 와 동일 
+        print('data_interval_start:' + str(kwargs.get('data_interval_start')))
+        print('data_interval_end:' + str(kwargs.get('data_interval_end')))
+        print('task_instance' + str(kwargs.get('ti')))
+
+    python_t1 > python_t1_2 >> python_function2()
