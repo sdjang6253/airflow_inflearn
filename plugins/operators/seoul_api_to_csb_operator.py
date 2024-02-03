001ef168f1869f83b2ccae6ac445fc25a2fff733
sdjang6253 <qkswl625341@gmail.com>
2024-02-03 17:36:50 +0900

    customoperator 오타 수정

--- plugins/operators/seoul_api_to_csb_operator.py
+++ plugins/operators/seoul_api_to_csb_operator.py
@@ -4,7 +4,7 @@ from airflow.hooks.base import BaseHook
 import pandas as pd
 
 class SeoulApiToCsvOperator(BaseOperator) :
-    template_fields = ('endpoint' , 'path' ,' file_name' , 'base_dt')   
+    template_fields = ('endpoint' , 'path' , 'file_name' , 'base_dt')   
     def __init__(self , dataset_nm , path , file_name , base_dt = None , **kwargs):
         super().__init__(**kwargs)
         self.http_conn_id = 'openapi.seoul.go.kr'
